7aad23d28cd0ac455d45dc6344a592e2c152db2d
Aditya <adityasara2622@gmail.com>
2026-05-23 18:51:37 +0530

    railway migration

---
+++ migrate.js
@@ -0,0 +1,29 @@
+require("dotenv/config");
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const { drizzle } = require("drizzle-orm/node-postgres");
+const { migrate } = require("drizzle-orm/node-postgres/migrator");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const db = drizzle(pool);
+
+async function main() {
+  console.log("Running migrations...");
+
+  await migrate(db, {
+    migrationsFolder: "./drizzle",
+  });
+
+  console.log("Migrations completed!");
+  process.exit(0);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
